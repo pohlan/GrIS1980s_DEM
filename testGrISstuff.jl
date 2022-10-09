bee3c769a1c9315dabc4f26fac2fb6a574ba7e82
Annegret Pohle <apohle@ethz.ch>
2022-10-09 00:16:31 -0800

    first try to formulate optimization problem

--- testGrISstuff.jl
+++ testGrISstuff.jl
@@ -1,12 +1,10 @@
-using NetCDF, Plots, LinearAlgebra
-
+using NetCDF, Plots, LinearAlgebra, Statistics, Optim
 file = "../Downloads/ex_gris_g1800m_v3a_rcp_26_id_0_2008_2300.nc"
-
-ncinfo(file)
-
-vel = ncread(file, "velsurf_mag") # doesn't seem to conatin any useful information
+# ncinfo(file)
 usurf = ncread(file, "usurf") # surface elevation
-ice_mass = ncread(file, "ice_mass")
+
+
+function make_objective(usurf)
 
 testgap = ones(size(usurf[:,:,1]))
 testgap[350:600,600:1150] .= NaN
@@ -16,9 +14,46 @@ testgap[400:500,430:600]  .= NaN
 u1 = usurf[:,:,1] .* testgap
 heatmap(u1')
 
-x1, x2, x3 = [vec(usurf[:,:,t]) for t = [80,90,100,200,250,292] ];
-x = [x1 x2 x3];
+I = vec(u1 .> 0)
+
+t = 100:50:200
+x = Float32[]
+x = vcat(x, vec(usurf[:,:,t[1]]))
+for i in 2:length(t)
+    x = hcat(x, vec(usurf[:,:,t[i]]))
+    I = hcat(I, trues(length(usurf[:,:,1]),1))
+end
+
+x_mean = mean(x,dims=2)
+x_std  = std(x,dims=2)
+R = (x .- x_mean) ./ (x_std .+ eps())
 
 # optimization problem
+λ_u = 10.0    # σ^2 / σ_u^2
+λ_v =  1.0    # σ^2 / σ_v^2
 
-U, S, V = svd(x);
+    # descent algorithm ...
+    function f(U_VT_)
+        U_ = U_VT_[1:size(R,1),:]
+        V_ = U_VT_[size(R,1)+1:end,:]'
+        # Predicted matrix
+        G = U_ * V_
+        # Data misfit
+        E_misfit = sum((R[I] .- G[I]).^2)           # mean?
+        # Regularization
+        E_reg = λ_u*sum(U_.^2) + λ_v*sum(V_.^2)   # divide by len(Rbar)?
+        # Sum to form total cost
+        E = E_misfit + E_reg #  + E_space + E_time
+        return E
+    end
+    return f, R
+end
+
+f, R = make_objective(usurf)
+
+U, S, V = svd(R);
+D = 6   # plot(S,yaxis=:log)
+U_ = 1e-2 .* randn(size(R,1),D)
+V_ = 1e-2 .* randn(D,size(R,2))
+x0 = [U_; V_']
+# optimize(f, x0)    # does not work
