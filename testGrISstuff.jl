eeeb678286351317bc7b514fd5e8c8cdcf907d4d
Annegret Pohle <apohle@ethz.ch>
2022-10-09 16:14:53 -0800

    first basic alg that's successful at reconstructing u1

--- testGrISstuff.jl
+++ testGrISstuff.jl
@@ -12,47 +12,51 @@ testgap[290:520,800:1240] .= NaN
 testgap[300:400,200:800]  .= NaN
 testgap[400:500,430:600]  .= NaN
 u1 = usurf[:,:,1] .* testgap
-heatmap(u1')
 
 Idx = vec(u1 .> 0)
 
-t = 150:50:292
+t = 150:13:292
 x = Float32[]
 x = vcat(x, vec(usurf[:,:,t[1]]))
 for i in 2:length(t)
     x = hcat(x, vec(usurf[:,:,t[i]]))
-    Idx = hcat(Idx, trues(length(usurf[:,:,1]),1))
 end
 
-x_mean = mean(x,dims=2)
-x_std  = std(x,dims=2)
-R = (x .- x_mean) ./ (x_std .+ eps())
-
-U, S, V = svd(R);
+x_mean = vec(mean(usurf,dims=3))
+x_std  = vec(std(usurf,dims=3))
+Xbar   = (x .- x_mean) ./ (x_std .+ eps())
+R      = (vec(u1) .- x_mean) ./ (x_std .+ eps())
+U, S, V = svd(Xbar);
 
 # optimization problem
 λ_u = 10.0    # σ^2 / σ_u^2
 λ_v =  1.0    # σ^2 / σ_v^2
 
     # descent algorithm ...
-    function f(V_)
+    function f(Vi_)
         # Predicted matrix
-        G = U * Diagonal(S) * V_'
+        G = U * Diagonal(S) * Vi_ # Vi: column vector of VT    #
         # Data misfit
-        E_misfit = sqrt(sum((R[Idx] .- G[Idx]).^2)) / sum(Idx)           # mean?
+        E_misfit = sum((R[Idx] .- G[Idx]).^2)           # mean?
         # Regularization
         # E_reg = λ_u*sum(U.^2) + λ_v*sum(V_.^2)   # divide by len(Rbar)?
         # Sum to form total cost
         E = E_misfit #+ E_reg #  + E_space + E_time
         return E
     end
-    return f, R
+    return f, Xbar, u1, R, x_mean, x_std, testgap
 end
 
-f, R = make_objective(usurf)
+f, Xbar, u1, R, x_mean, x_std, testgap = make_objective(usurf)
 
 D = 6   # plot(S,yaxis=:log)
 # U_ = 1e-2 .* randn(size(R,1),D)Infil
-V_ = 1e-2 .* randn(size(R,2),size(R,2))
-f(V_)
-out = optimize(f, V_)
+Vi_ = 1e-2 .* randn(size(Xbar,2),1)
+f(Vi_)
+out = optimize(f, Vi_)
+
+v_est = out.minimizer
+U, S, V = svd(Xbar)
+xbar_est = U * Diagonal(S) * v_est
+x_est = reshape(xbar_est .* x_std .+ x_mean,size(u1))
+heatmap(((x_est.-usurf[:,:,1]).*isnan.(testgap))')
