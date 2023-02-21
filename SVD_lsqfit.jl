297a8da03dc4643311b63a2462fdc02b7c45856e
Annegret <annegret.pohle@outlook.com>
2023-02-21 13:23:37 -0900

    two ways to do indexing in lsqfit.. not quite sure yet what's correct

--- SVD_lsqfit.jl
+++ SVD_lsqfit.jl
@@ -1,5 +1,5 @@
-using NetCDF, Statistics, LinearAlgebra, Glob, PyPlot, Printf
-
+using NetCDF, Statistics, LinearAlgebra, Glob, PyPlot, Printf, JLD2, TSVD
+close("all")
 # load model data
 include("read_in.jl")
 model_files = glob("data/usurf_ex_gris_g1800m_v5_RAGIS_id_*.nc") # 60 time steps in total, 27 files
@@ -11,20 +11,26 @@ Data_mean = mean(Data, dims=2)
 Data_centr = Data .- Data_mean
 
 println("Computing the SVD..")
-U, S, V = svd(Data_centr)
+U, S, V = tsvd(Data_centr, 60)
 
 # load observations
 obs       = ncread(model_files[27],"usurf")[:,:,1]
-obs_flat  = reshape(obs_artif, nx * ny, 1)
+# obs       = ncread("data/aerodem_gris_g1800.nc", "Band1")
+obs_flat  = reshape(obs, nx * ny, 1)
 b         = obs_flat .- Data_mean
+Id, I_intr = load("data/data1980s_indices.jld2","I_marg","I_intr")
 
 # solve the lsqfit problem
-λ     = 0
+λ     = 10^7
 denom = S.^2 .+ λ
-w     = S .* transpose(U)*b ./ denom
+US    = U*diagm(S)
+w     = transpose(US[Id,:]) * b[Id] ./ denom
+# U_A, Σ_A, V_A = svd(US[Id,:])
+# w     = V_A * diagm(Σ_A) * transpose(U_A) * b[Id] ./ denom  # this gives the exact same result as the first option to calculate w
 x̂     = U*diagm(S)*w .+ Data_mean
 
 dif   = x̂ .- obs_flat
+# dif[I_intr] .= 0.
 @printf("L2 error: %1.1f\n", norm(dif))
 
 figure()
