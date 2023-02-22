c624ca7a817e0dd7989209f401bf701a819b7a85
Annegret <annegret.pohle@outlook.com>
2023-02-22 14:57:59 -0900

    write up tex file for lsqfit solution and make jl file consistent

--- SVD_lsqfit.jl
+++ SVD_lsqfit.jl
@@ -1,9 +1,14 @@
 using NetCDF, Statistics, LinearAlgebra, Glob, PyPlot, Printf, JLD2, TSVD
 close("all")
+
+# parameters
+r = 80            # truncation of the SVD
+λ = 10^7          # regularization
+
 # load model data
 include("read_in.jl")
 model_files = glob("data/usurf_ex_gris_g1800m_v5_RAGIS_id_*.nc") # 60 time steps in total, 27 files
-Data, x, y, t, nx, ny = read_model_data(files=1:10,tsteps=1:60;model_files)
+Data, x, y, t, nx, ny = read_model_data(files=2:27,tsteps=2:60;model_files)
 Data = permutedims(Data)
 
 # center the data
@@ -11,25 +16,26 @@ Data_mean = mean(Data, dims=2)
 Data_centr = Data .- Data_mean
 
 println("Computing the SVD..")
-U, S, V = tsvd(Data_centr, 60)
+U, Σ, V = tsvd(Data_centr, r)
 
 # load observations
-obs       = ncread(model_files[27],"usurf")[:,:,1]
-# obs       = ncread("data/aerodem_gris_g1800.nc", "Band1")
-obs_flat  = reshape(obs, nx * ny, 1)
-b         = obs_flat .- Data_mean
+obs        = ncread(model_files[1],"usurf")[:,:,1]            # reconstruct a model geometry
+# obs       = ncread("data/aerodem_gris_g1800.nc", "Band1")   # actual aerodem data
+obs_flat   = reshape(obs, nx * ny, 1)
+x_data     = obs_flat .- Data_mean
+if !isfile("data/aerodem_data_indices.jld2")
+    include("save_indices.jl")
+end
 Id, I_intr = load("data/data1980s_indices.jld2","I_marg","I_intr")
 
 # solve the lsqfit problem
-λ     = 10^7
-denom = S.^2 .+ λ
-US    = U*diagm(S)
-w     = transpose(US[Id,:]) * b[Id] ./ denom
-# U_A, Σ_A, V_A = svd(US[Id,:])
-# w     = V_A * diagm(Σ_A) * transpose(U_A) * b[Id] ./ denom  # this gives the exact same result as the first option to calculate w
-x̂     = U*diagm(S)*w .+ Data_mean
+UΣ            = U*diagm(Σ)
+U_A, Σ_A, V_A = svd(UΣ[Id,:])
+D             = transpose(diagm(Σ_A))*diagm(Σ_A) + λ*I
+v_rec         = V_A * D^(-1) * transpose(diagm(Σ_A)) * transpose(U_A) * x_data[Id] # this gives the exact same result as the first option to calculate w
+x_rec         = U*diagm(Σ)*v_rec .+ Data_mean
 
-dif   = x̂ .- obs_flat
+dif   = x_rec .- obs_flat
 # dif[I_intr] .= 0.
 @printf("L2 error: %1.1f\n", norm(dif))
 
