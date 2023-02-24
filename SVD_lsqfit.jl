b2b056393f1a09e50abebb3d04c23d9fa793a7a4
Annegret <annegret.pohle@outlook.com>
2023-02-23 18:48:03 -0900

    make code run on 1200m resolution

--- SVD_lsqfit.jl
+++ SVD_lsqfit.jl
@@ -4,10 +4,11 @@ close("all")
 # parameters and paths to adjust
 r           = 40            # truncation of the SVD
 λ           = 10^7          # regularization
-model_files = glob("data/usurf_ex_gris_g1800m_v5_RAGIS_id_*.nc") # 60 time steps in total, 27 files
+res         = "1200"
+model_files = glob("data/usurf_ex_gris_g" * res * "m_v2023_RAGIS_id_*.nc") # 60 time steps in total, 27 files
 
 # load masks
-I_no_ocean, I_data, I_intr = load("data/aerodem_data_indices.jld2","I_not_ocean","I_marg","I_intr")
+I_no_ocean, I_data, I_intr = load("data/indices_aerodem_g" * res * "m.jld2","I_not_ocean","I_marg","I_intr")
 
 # load model data
 Data_all, nx, ny = read_model_data(which_files=2:27,tsteps=2:60;model_files)
@@ -18,7 +19,8 @@ Data_centr = Data .- Data_mean
 
 # load observations
 obs        = ncread(model_files[1],"usurf")[:,:,1]            # reconstruct a model geometry
-# obs       = ncread("data/aerodem_gris_g1800.nc", "Band1")   # actual aerodem data
+# obs       = ncread("data/aerodem_g" * res * "m_geoid_corrected_1978_1987_mean.nc", "surface_altitude")   # actual aerodem data
+# obs        = ncread("data/pism_Greenland_" * res * "m_mcb_jpl_v2023_RAGIS_ctrl.nc", "surface")
 obs_flat   = reshape(obs, nx * ny, 1)[I_no_ocean]
 x_data     = obs_flat .- Data_mean
 
@@ -34,11 +36,11 @@ D             = transpose(diagm(Σ_A))*diagm(Σ_A) + λ*I
 v_rec         = V_A * D^(-1) * transpose(diagm(Σ_A)) * transpose(U_A) * x_data[I_data]
 x_rec         = U*diagm(Σ)*v_rec .+ Data_mean
 
-dif              = zeros(nx*ny)
-dif[I_no_ocean] .= x_rec .- obs_flat
-# dif[I_intr] .= 0.
+dif                      = zeros(nx*ny)
+dif[I_no_ocean[I_data]] .= (x_rec .- obs_flat)[I_data]
+dif[I_no_ocean[I_intr]] .= (x_rec .- obs_flat)[I_intr]
 @printf("L2 error: %1.1f\n", norm(dif))
 
 figure()
-p = pcolormesh(reshape(dif,nx,ny)',cmap="bwr"); colorbar(label="[m]"); p.set_clim(-10,10)
+p = pcolormesh(reshape(dif,nx,ny)',cmap="bwr"); colorbar(label="[m]"); p.set_clim(-50,50)
 title("reconstructed - true")
--- src/save_indices.jl
+++ src/save_indices.jl
@@ -1,41 +1,47 @@
 using JLD2, NetCDF
 
-# load bedmachine mask
-mask            = ncread("data/pism_Greenland_1800m_mcb_jpl_v2023_RAGIS_ctrl.nc", "mask")
-nx, ny          = size(mask)
-not_ocean_mask  = reshape(mask.!= 0. .&& mask .!= 4, nx*ny)  # mask for model grid point that are neither ocean nor outside Greenland
-I_not_ocean     = findall(not_ocean_mask)                    # non-ocean indices
-mask_ice_nested = reshape(mask,nx*ny)[I_not_ocean] .== 2     
-
-# load the aerodem observations
-obs_file ="data/aerodem_1978_1987_wgs84_g1800m.nc"
-obs      = ncread(obs_file, "surface_altitude")
-R        = reshape(obs, length(obs))[I_not_ocean]
-
-# get indices where there is data and ice, with respect to non-ocean-mask
-I_marg    = findall(R .!= -9999 .&& mask_ice_nested)
-# get indices of the interior where there is ice and but no data, with respect to non-ocean-mask
-bool_intr = obs.==-9999
-for iy = 1:ny
-    ix = 1
-    while obs[ix,iy] .== -9999
-        bool_intr[ix,iy] = false
-        ix += 1
-        if ix > nx
-            break
-        end
-    end
-    ix = size(obs,1)
-    while obs[ix,iy] .== -9999
-        bool_intr[ix,iy] = false
-        ix -= 1
-        if ix < 1
-            break
-        end
-    end
-end
-I_intr = findall(reshape(bool_intr,nx*ny,1)[I_not_ocean] .&& mask_ice_nested) ####### mask = ice, data = no interior of ice sheet
-
-# save
 mkpath("data/")
-jldsave("data/aerodem_data_indices.jld2"; I_not_ocean, I_marg, I_intr)
+
+
+resolutions = ["1200", "1800"]
+for res in resolutions
+
+    # load bedmachine mask
+    mask            = ncread("data/pism_Greenland_" * res * "m_mcb_jpl_v2023_RAGIS_ctrl.nc", "mask")
+    nx, ny          = size(mask)
+    not_ocean_mask  = reshape(mask.!= 0. .&& mask .!= 4, nx*ny)  # mask for model grid point that are neither ocean nor outside Greenland
+    I_not_ocean     = findall(not_ocean_mask)                    # non-ocean indices
+    mask_ice_nested = reshape(mask,nx*ny)[I_not_ocean] .== 2     
+
+    # load the aerodem observations
+    obs_file ="data/aerodem_g" * res * "m_geoid_corrected_1978_1987_mean.nc"
+    obs      = ncread(obs_file, "surface_altitude")
+    R        = reshape(obs, length(obs))[I_not_ocean]
+
+    # get indices where there is data and ice, with respect to non-ocean-mask
+    I_marg    = findall(R .!= -9999 .&& mask_ice_nested)
+    # get indices of the interior where there is ice and but no data, with respect to non-ocean-mask
+    bool_intr = obs.==-9999
+    for iy = 1:ny
+        ix = 1
+        while obs[ix,iy] .== -9999
+            bool_intr[ix,iy] = false
+            ix += 1
+            if ix > nx
+                break
+            end
+        end
+        ix = size(obs,1)
+        while obs[ix,iy] .== -9999
+            bool_intr[ix,iy] = false
+            ix -= 1
+            if ix < 1
+                break
+            end
+        end
+    end
+    I_intr = findall(reshape(bool_intr,nx*ny,1)[I_not_ocean] .&& mask_ice_nested) ####### mask = ice, data = no interior of ice sheet
+
+    # save
+    jldsave("data/indices_aerodem_g" * res * "m.jld2"; I_not_ocean, I_marg, I_intr)
+end
