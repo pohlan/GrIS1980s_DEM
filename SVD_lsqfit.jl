849df3361a3b5faabe936bb7fd8b38de1cf2ef47
Annegret <annegret.pohle@outlook.com>
2023-02-24 17:02:23 -0900

    added command line arguments

--- SVD_lsqfit.jl
+++ SVD_lsqfit.jl
@@ -1,18 +1,20 @@
 using svd_IceSheetDEM, NetCDF, Statistics, LinearAlgebra, Glob, PyPlot, Printf, JLD2, TSVD
-close("all")
 
-# parameters and paths to adjust
-F           = Float32       # Julia default is Float64 but that kills the process for the full training data set if r is too large
-r           = 40            # truncation of the SVD
-λ           = 10^7          # regularization
-res         = "1200"
-model_files = glob("data/usurf_ex_gris_g" * res * "m_v2023_RAGIS_id_*.nc") # 60 time steps in total, 27 files
+# retrieve command line arguments
+parsed_args = parse_commandline()
+r        = parsed_args["r"]        # truncation of the SVD
+λ        = parsed_args["λ"]        # regularization
+res      = parsed_args["res"]      # resolution
+filepath = parsed_args["filepath"]
+  
+F        = Float32 # Julia default is Float64 but that kills the process for the full training data set if r is too large
 
 # load masks
 I_no_ocean, I_data, I_intr = load("data/indices_aerodem_g" * res * "m.jld2","I_not_ocean","I_marg","I_intr")
 
 # load model data
-Data_all, nx, ny = read_model_data(which_files=1:27,tsteps=1:1;F,model_files)
+model_files = glob(joinpath(filepath,"usurf_ex_gris_g" * res * "m_v2023_RAGIS_id_*.nc")) # 60 time steps in total, 27 files
+Data_all, nx, ny = read_model_data(which_files=1:1,tsteps=1:20;F,model_files)
 # centering
 Data       = Data_all[I_no_ocean, :]   # remove cells where there is ocean, saves half of the space
 Data_mean  = mean(Data, dims=2)
@@ -20,12 +22,15 @@ Data_centr = Data .- Data_mean
 
 # compute SVD
 println("Computing the SVD..")
-U, Σ, V = tsvd(Data_centr, r)
-
+if r === nothing
+    U, Σ, V = svd(Data_centr)      # full SVD
+else
+    U, Σ, V = tsvd(Data_centr, r)
+end
 # load observations
 # obs        = ncread(model_files[1],"usurf")[:,:,1]            # reconstruct a model geometry
-# obs       = ncread("data/aerodem_g" * res * "m_geoid_corrected_1978_1987_mean.nc", "surface_altitude")   # actual aerodem data
-obs        = ncread("data/pism_Greenland_" * res * "m_mcb_jpl_v2023_RAGIS_ctrl.nc", "surface")
+# obs       = ncread(filepath * "aerodem_g" * res * "m_geoid_corrected_1978_1987_mean.nc", "surface_altitude")   # actual aerodem data
+obs        = ncread(filepath * "pism_Greenland_" * res * "m_mcb_jpl_v2023_RAGIS_ctrl.nc", "surface")
 obs_flat   = F.(reshape(obs, nx * ny, 1)[I_no_ocean])
 x_data     = obs_flat .- Data_mean
 
@@ -42,6 +47,26 @@ dif[I_no_ocean[I_data]] .= (x_rec .- obs_flat)[I_data]
 dif[I_no_ocean[I_intr]] .= (x_rec .- obs_flat)[I_intr]
 @printf("L2 error: %1.1f\n", norm(dif))
 
-figure()
-p = pcolormesh(reshape(dif,nx,ny)',cmap="bwr"); colorbar(label="[m]"); p.set_clim(-50,50)
-title("reconstructed - true")
+# close("all")
+# figure()
+# p = pcolormesh(reshape(dif,nx,ny)',cmap="bwr"); colorbar(label="[m]"); p.set_clim(-50,50)
+# title("reconstructed - true")
+
+# save as nc file
+if parsed_args["save"]
+    println("Saving file..")
+    filename = "data/dem_ragis_r_$r _lambda_$λ.nc"
+    varname  = "usurf"
+    attribs  = Dict("units"   => "m",
+                    "data_min" => 0.0)
+    x    = ncread(model_files[1], "x")
+    y    = ncread(model_files[1], "y")
+    time = ncread(model_files[1], "time")
+    data_rec = zeros(nx*ny)
+    data_rec[I_no_ocean] = x_rec
+    if isfile(filename)
+        run(`rm $filename`)
+    end
+    nccreate(filename, varname, "x", x, "y", y, "time", 1, atts=attribs)
+    ncwrite(reshape(data_rec,nx,ny,1), filename, varname)
+end
--- src/svd_IceSheetDEM.jl
+++ src/svd_IceSheetDEM.jl
@@ -1,9 +1,41 @@
 __precompile__(false)
 module svd_IceSheetDEM
 
-using JLD2, NetCDF, Glob, ProgressMeter
+using JLD2, NetCDF, Glob, ProgressMeter, ArgParse
 
-export read_model_data
+export read_model_data, parse_commandline
+
+function parse_commandline()
+    s = ArgParseSettings()
+    @add_arg_table s begin
+        "--λ", "--lambda"
+            help     = "regularization parameter for the least squares problem"
+            arg_type = Float64
+            default  = 0.
+        "--r", "--trunc"
+            help     = "number of modes that should be kept in the SVD truncation"
+            arg_type = Int
+            default  = nothing
+        "--res"
+            help     = "fesolution in m, currently available at '1200' or '1800'"
+            arg_type = String
+            default  = "1200"
+        "--filepath"
+            help     = "folder where the training data 'usurf_*' is stored"
+            arg_type = String
+            default  = "data/"
+        "--save"
+            help     = "save the output in an .nc file (option takes no argument)"
+            action   = :store_true
+        # doesn't currently work if one wants to pass an actual argument, for --F "Float32" it gives the error
+        # invalid argument: Float32 (conversion to type DataType failed; you may need to overload ArgParse.parse_item;
+        # "--F"
+        #     help     = "Precision, Julia default is Float64 but that kills the process for the full training data set if r is too large"
+        #     arg_type = DataType
+        #     default  = Float32
+    end
+    return parse_args(s)
+end
 
 include("read_in.jl")
 
