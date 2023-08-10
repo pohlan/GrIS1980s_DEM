a86d49aa7afb868479190c671ffc70ce932a9f7c
Annegret Pohle <annegret@system76-pc.localdomain>
2023-08-10 15:08:10 -0800

    fixed 'No usable source images.' error and only downloading 1981 aerodems now, adjusted shapefile

--- test/runtests.jl
+++ test/runtests.jl
@@ -37,11 +37,11 @@ end
 
 const gr = 4000
 
-imbie_path          = "../data/gris-imbie-1980/"
-aerodem_path        = "../data/aerodem/"
+imbie_path          = "data/gris-imbie-1980/"
+aerodem_path        = "data/aerodem/"
 aero_150            = aerodem_path*"aerodem_rm-filtered_geoid-corr_g150.nc"
 aero_gr             = aerodem_path*"aerodem_rm-filtered_geoid-corr_g$(gr).nc"
-bedmachine_path     = "../data/bedmachine/"
+bedmachine_path     = "data/bedmachine/"
 bedmachine_file_gr  = bedmachine_path*"bedmachine_g$(gr).nc"
 template_file       = "testdata/testtemplate_g$(gr).nc"
 shp_file            = "testdata/testshape.shp"
@@ -61,19 +61,19 @@ end
 
 @testset "aerodem" begin
     rm(aerodem_path, recursive=true, force=true)
-    create_aerodem(aerodem_path, shp_file, bedmachine_path)
+    create_aerodem(aerodem_path, shp_file, bedmachine_path, "1981")    # only download the DEMs from 1981 while testing to save some space
     gdalwarp(aero_150; gr, srcnodata="0.0", dest=aero_gr)
     ds150 = ncread(aero_150, "surface")
     dsgr  = ncread(aero_gr , "Band1")
-    @test sum(ds150 .> 0) == 4642335 && maximum(ds150) == 2442.629150390625
-    @test sum(dsgr  .> 0) == 9536    && maximum(dsgr)  == 2128.8025406615666
+    @test sum(ds150 .> 0) == 2094088 && maximum(ds150) == 3685.548095703125
+    @test sum(dsgr  .> 0) == 7024    && maximum(dsgr)  == 3252.3076520647323
 end
 
 @testset "imbie mask" begin
     rm(imbie_path, recursive=true, force=true)
     create_imbie_mask(gr; imbie_path, imbie_shp_file=shp_file, sample_path=aero_150)
     imb = ncread(imbie_mask, "Band1")
-    @test sum(imb .== 1) == 25953
+    @test sum(imb .== 1) == 9496
 end
 
 ###############################
@@ -87,12 +87,12 @@ end
     r           = 1e3
     rec_file    = solve_lsqfit(F, λ, r, gr, imbie_mask, [template_file], aero_gr)
     rec         = ncread(rec_file, "surface")
-    @test maximum(rec) ≈ 2489f0 && rec[322:324,427] ≈ Float32[345.6, 345.6, 84.7] && sum(rec .> 0) == 18122
+    @test maximum(rec) ≈ 3042.707f0 && rec[200:203, 80] ≈ Float32[62.8, 62.8, 89.6, 147.5] && sum(rec .> 0) == 9144
     create_reconstructed_bedmachine(rec_file, bedmachine_file_gr)
     bm          = NCDataset("output/bedmachine1980_reconstructed_g$(gr).nc")
     @test all(["mask","bed","surface","thickness","polar_stereographic","x","y"] .∈ (keys(bm),))
     mask = bm["mask"][:]
-    @test sum(mask.==1) == 143784 && sum(mask.==2) == 12106 && sum(mask.==3) == 554
-    @test isapprox(maximum(bm["surface"][:]), 2489, atol=0.5)
+    @test sum(mask.==1) == 144046 && sum(mask.==2) == 5824 && sum(mask.==3) == 135
+    @test isapprox(maximum(bm["surface"][:]), 3043, atol=1)
     close(bm)
 end
