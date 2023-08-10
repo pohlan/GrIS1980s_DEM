9439ec80f38798e2d964c4e87f5d0c8d9c78be2b
Annegret Pohle <annegret@system76-pc.localdomain>
2023-08-10 11:32:00 -0800

    uncommented everything

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using svd_IceSheetDEM
 using Test, TSVD, LinearAlgebra, NetCDF, NCDatasets
 
+cd(@__DIR__)  # set working directory to where file is located
+
 #####################################
 # testing the randomized svd (rsvd) #
 #####################################
@@ -46,7 +48,7 @@ shp_file            = "testdata/testshape.shp"
 imbie_mask          = imbie_path * "imbie_mask_g$(gr).nc"
 
 @testset "bedmachine" begin
-    rm(bedmachine_path*"BedMachineGreenland-v5.nc", force=true)
+    rm(bedmachine_path, recursive=true, force=true)
     create_bedmachine_grid(gr, bedmachine_path, template_file)
     ds = NCDataset(bedmachine_file_gr)
     @test all(["mask","geoid","bed","surface","thickness"] .∈ (keys(ds),))
@@ -57,40 +59,40 @@ imbie_mask          = imbie_path * "imbie_mask_g$(gr).nc"
     close(ds)
 end
 
-# @testset "aerodem" begin
-#     rm(aero_150, force=true)
-#     rm(aero_gr , force=true)
-#     create_aerodem(aerodem_path, shp_file, bedmachine_path)
-#     gdalwarp(aero_150; gr, srcnodata="0.0", dest=aero_gr)
-#     ds150 = ncread(aero_150, "surface")
-#     dsgr  = ncread(aero_gr , "Band1")
-#     @test sum(ds150 .> 0) == 4642335 && maximum(ds150) == 2442.629150390625
-#     @test sum(dsgr  .> 0) == 9536    && maximum(dsgr)  == 2128.8025406615666
-# end
+@testset "aerodem" begin
+    rm(aerodem_path, recursive=true, force=true)
+    create_aerodem(aerodem_path, shp_file, bedmachine_path)
+    gdalwarp(aero_150; gr, srcnodata="0.0", dest=aero_gr)
+    ds150 = ncread(aero_150, "surface")
+    dsgr  = ncread(aero_gr , "Band1")
+    @test sum(ds150 .> 0) == 4642335 && maximum(ds150) == 2442.629150390625
+    @test sum(dsgr  .> 0) == 9536    && maximum(dsgr)  == 2128.8025406615666
+end
 
-# @testset "imbie mask" begin
-#     rm(imbie_mask, force=true)
-#     create_imbie_mask(gr; imbie_path, imbie_shp_file=shp_file, sample_path=aero_150)
-#     imb = ncread(imbie_mask, "Band1")
-#     @test sum(imb .== 1) == 25953
-# end
+@testset "imbie mask" begin
+    rm(imbie_path, recursive=true, force=true)
+    create_imbie_mask(gr; imbie_path, imbie_shp_file=shp_file, sample_path=aero_150)
+    imb = ncread(imbie_mask, "Band1")
+    @test sum(imb .== 1) == 25953
+end
 
 ###############################
 # testing the problem solving #
 ###############################
 
-# @testset "solve least square fit" begin
-#     F           = Float32
-#     λ           = 1e5
-#     r           = 1e3
-#     rec_file    = solve_lsqfit(F, λ, r, gr, imbie_mask, [template_file], aero_gr)
-#     rec         = ncread(rec_file, "surface")
-#     @test maximum(rec) ≈ 2489f0 && rec[322:324,427] ≈ Float32[345.6, 345.6, 84.7] && sum(rec .> 0) == 18122
-#     create_reconstructed_bedmachine(rec_file, bedmachine_file_gr)
-#     bm          = NCDataset("output/bedmachine1980_reconstructed_g$(gr).nc")
-#     @test all(["mask","bed","surface","thickness","polar_stereographic","x","y"] .∈ (keys(bm),))
-#     mask = bm["mask"][:]
-#     @test sum(mask.==1) == 143784 && sum(mask.==2) == 12106 && sum(mask.==3) == 554
-#     @test isapprox(maximum(bm["surface"][:]), 2489, atol=0.5)
-#     close(bm)
-# end
+@testset "solve least square fit" begin
+    rm("../output/", recursive=true, force=true)
+    F           = Float32
+    λ           = 1e5
+    r           = 1e3
+    rec_file    = solve_lsqfit(F, λ, r, gr, imbie_mask, [template_file], aero_gr)
+    rec         = ncread(rec_file, "surface")
+    @test maximum(rec) ≈ 2489f0 && rec[322:324,427] ≈ Float32[345.6, 345.6, 84.7] && sum(rec .> 0) == 18122
+    create_reconstructed_bedmachine(rec_file, bedmachine_file_gr)
+    bm          = NCDataset("output/bedmachine1980_reconstructed_g$(gr).nc")
+    @test all(["mask","bed","surface","thickness","polar_stereographic","x","y"] .∈ (keys(bm),))
+    mask = bm["mask"][:]
+    @test sum(mask.==1) == 143784 && sum(mask.==2) == 12106 && sum(mask.==3) == 554
+    @test isapprox(maximum(bm["surface"][:]), 2489, atol=0.5)
+    close(bm)
+end
