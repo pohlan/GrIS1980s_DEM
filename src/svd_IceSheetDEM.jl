7f18498c4b3bfa8106c354c0fde854484118e809
pohlan <annegret.pohle@outlook.com>
2024-02-22 14:10:22 -0900

    separated time intensive SVD from part where parameters r and lambda come in, makes grid search much more efficient

--- src/svd_IceSheetDEM.jl
+++ src/svd_IceSheetDEM.jl
@@ -17,7 +17,7 @@ import Plots, StatsPlots
 export parse_commandline
 export archgdal_read, gdalwarp
 export create_aerodem, create_bedmachine_grid, create_imbie_mask, create_atm_grid, create_dhdt_grid
-export solve_lsqfit, create_reconstructed_bedmachine
+export do_reconstruction, create_reconstructed_bedmachine
 export residual_analysis
 
 function parse_commandline(args)
@@ -52,7 +52,6 @@ function parse_commandline(args)
     return parse_args(args,s)
 end
 
-include("read_in.jl")
 include("gdal_helpers.jl")
 include("reconstruction_routines.jl")
 include("statistics_helpers.jl")
